61ceff88800acc5b3bfb4a03cdf23f622204fcec
Thiruppathi Muthukumar <thiru.androider@gmail.com>
2015-11-22 20:49:33 +0000

    initial js files, addtnl gif and pics

---
+++ tDrone.js
@@ -0,0 +1,39 @@
+var arDrone = require('ar-drone');
+var tDrone = arDrone.createClient();
+
+tDrone.createRepl();
+
+/*
+tDrone.takeoff();
+
+tDrone
+  .after(5000, function() {
+    this.clockwise(0.5);
+  })
+  .after(3000, function() {
+    this.animate('flipLeft', 15);
+  })
+  .after(1000, function() {
+    this.stop();
+    this.land();
+  });
+*/
+
+/*
+takeoff()
+
+stop()
+
+
+front(0.1)
+
+stop()
+
+back(0.1)
+
+stop()
+
+animate('flipLeft', 15)
+
+animate('flipAhead', 15)
+*/
